2229717f4531a146b427c0c29c156854d77225be
maprevidelli <marco.previdelli@gmail.com>
2025-04-22 05:49:39 -0300

    interação MK

--- hello_git.py
+++ hello_git.py
@@ -1,3 +1,9 @@
 print(f'Hello Git world!')
 print('Test feito em uma segunda Branch -> ramo1')
+nome = input('Enter your name please: ')
+nome = nome.lower()
+if nome == 'marco':
+	print(f'Hey, {nome}! You are de supreme Mortal Kombat Warrior!')
+else:	
+	print(f'{nome} vc cuzão..')
 
